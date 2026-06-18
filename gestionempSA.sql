b70a3f62d9ffbf7756c158146dcb066ec1752254
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-18 10:50:08 -0600

    Creación de Cargos y empleados

--- gestionempSA.sql
+++ gestionempSA.sql
@@ -17,7 +17,35 @@ CREATE SCHEMA GestionEmpresa
 GO --1. Gestión de Departamentos
 
 CREATE SCHEMA GestionEmpleados
-GO --2. Gestión de Cargos, 3. Gestión de Empleados, 4. Gestión de Contratos, 5. Gestión de Asistencia, 6. Gestión de Evaluaciones de Desempeño
+GO --4. Gestión de Contratos, 5. Gestión de Asistencia, 6. Gestión de Evaluaciones de Desempeño
 
 CREATE SCHEMA GestionCapacitaciones
 GO	--7. Gestión de Capacitaciones, INTERMEDIA: Empleado_Capacitación
+
+CREATE TABLE GestionEmpleados.Cargos (
+	idCargo INT IDENTITY(1,1) CONSTRAINT PK_idcargo PRIMARY KEY
+	, nombre NVARCHAR(40) NOT NULL
+	, descripcion NVARCHAR(120) NOT NULL
+	, salarioBase DECIMAL(6,2) NOT NULL
+	, nivel NVARCHAR(40) NOT NULL
+	, createdAt DATE DEFAULT GETDATE()
+	, updatedAt DATE DEFAULT GETDATE()
+	, deletedAt DATE NULL
+)
+GO
+
+CREATE TABLE GestionEmpleados.Empleados (
+	idEmpleado INT IDENTITY(1,1) CONSTRAINT PK_idempleado PRIMARY KEY
+	, identificacion NVARCHAR(20) NOT NULL
+	, nombres NVARCHAR(50) NOT NULL
+	, apellidos NVARCHAR(50) NOT NULL
+	, fechaNac DATE NOT NULL
+	, sexo CHAR NOT NULL
+	, telefono NVARCHAR(20) NOT NULL
+	, correo NVARCHAR(80) NOT NULL
+	, direccion NVARCHAR(120) NOT NULL
+	, fechaContratacion DATE DEFAULT GETDATE()
+	, estadoLaboral NVARCHAR(30) NOT NULL
+	, 
+)
+GO
