692b73be9c40c8cf5b084746d978dcd0e9c8e522
andreadelaroca <asodelaroca@uamv.edu.ni>
2026-06-18 11:29:32 -0600

    Correcciones de normalización

--- gestionempSA.sql
+++ gestionempSA.sql
@@ -14,7 +14,13 @@ USE gestionempSA
 GO
 
 CREATE SCHEMA GestionEmpresa
-GO --1. Gestión de Departamentos
+GO
+
+CREATE SCHEMA GestionEmpleados
+GO
+
+CREATE SCHEMA GestionCapacitaciones
+GO
 
 CREATE TABLE GestionEmpresa.Departamentos (
 	idDepartamento INT IDENTITY(1,1) CONSTRAINT PK_iddepartamento PRIMARY KEY
@@ -26,14 +32,8 @@ CREATE TABLE GestionEmpresa.Departamentos (
 	, updatedAt DATE DEFAULT GETDATE()
 	, deletedAt DATE NULL
 	, CONSTRAINT CHK_dept_estado CHECK (estado IN ('Activo', 'Inactivo'))
-);
+)
 GO
-
-CREATE SCHEMA GestionEmpleados
-GO --4. Gestión de Contratos, 5. Gestión de Asistencia, 6. Gestión de Evaluaciones de Desempeño
-
-CREATE SCHEMA GestionCapacitaciones
-GO	--7. Gestión de Capacitaciones, INTERMEDIA: Empleado_Capacitación
 
 CREATE TABLE GestionEmpleados.Cargos (
 	idCargo INT IDENTITY(1,1) CONSTRAINT PK_idcargo PRIMARY KEY
@@ -59,141 +59,93 @@ CREATE TABLE GestionEmpleados.Empleados (
 	, direccion NVARCHAR(120) NOT NULL
 	, fechaContratacion DATE DEFAULT GETDATE()
 	, estadoLaboral NVARCHAR(30) NOT NULL
-	, 
+	, idDepartamento INT CONSTRAINT FK_iddep FOREIGN KEY REFERENCES GestionEmpresa.Departamentos(idDepartamento)
+    , idCargo INT CONSTRAINT FK_idcargo FOREIGN KEY REFERENCES GestionEmpleados.Cargos(idCargo)
 )
 GO
 
--- =========================================================================
--- 4. GESTIÓN DE CONTRATOS
--- =========================================================================
 CREATE TABLE GestionEmpresa.Contratos (
-    CodigoContrato INT IDENTITY(1,1),
-    TipoContrato VARCHAR(50) NOT NULL,
-    FechaInicio DATE NOT NULL,
-    FechaFinalizacion DATE NULL,
-    SalarioAcordado DECIMAL(12,2) NOT NULL,
-    EstadoContrato VARCHAR(20) DEFAULT 'Activo',
-    CodigoEmpleado INT NOT NULL,
-    createdAt DATE DEFAULT GETDATE()
+    idContrato INT IDENTITY(1,1),
+    tipo VARCHAR(50) NOT NULL,
+    fechaInicio DATE NOT NULL,
+    fechaFinalizacion DATE NULL,
+    salarioAcordado DECIMAL(12,2) NOT NULL,
+    estado VARCHAR(20) DEFAULT 'Activo',
+    idEmpleado INT CONSTRAINT FK_idemp FOREIGN KEY REFERENCES GestionEmpleados.Empleados(idEmpleado)
+    , createdAt DATE DEFAULT GETDATE()
 	, updatedAt DATE DEFAULT GETDATE()
 	, deletedAt DATE NULL
     
-    CONSTRAINT PK_Contratos PRIMARY KEY (CodigoContrato),
-    CONSTRAINT CHK_Contrato_Tipo CHECK (TipoContrato IN ('Temporal', 'Permanente', 'Por proyecto')),
-    CONSTRAINT CHK_Contrato_Salario CHECK (SalarioAcordado > 0),
-    CONSTRAINT CHK_Contrato_Estado CHECK (EstadoContrato IN ('Activo', 'Vencido', 'Rescindido')),
-    CONSTRAINT FK_Contratos_Empleados FOREIGN KEY (CodigoEmpleado) 
-        REFERENCES GestionEmpleados.Empleados(CodigoEmpleado)
-);
+    CONSTRAINT PK_Contratos PRIMARY KEY (idContrato),
+    CONSTRAINT CHK_Contrato_Tipo CHECK (tipo IN ('Temporal', 'Permanente', 'Por proyecto')),
+    CONSTRAINT CHK_Contrato_Salario CHECK (salarioAcordado > 0),
+    CONSTRAINT CHK_Contrato_Estado CHECK (estado IN ('Activo', 'Vencido', 'Rescindido')),
+)
 GO
 
--- =========================================================================
--- 5. GESTIÓN DE ASISTENCIA
--- =========================================================================
 CREATE TABLE GestionEmpresa.Asistencia (
-    CodigoAsistencia INT IDENTITY(1,1),
-    Fecha DATE NOT NULL,
-    HoraEntrada TIME NOT NULL,
-    HoraSalida TIME NULL,
-    EstadoAsistencia VARCHAR(20) NOT NULL,
-    CodigoEmpleado INT NOT NULL,
+    idAsistencia INT IDENTITY(1,1),
+    fecha DATE NOT NULL,
+    horaEntrada TIME NOT NULL,
+    horaSalida TIME NULL,
+    estado VARCHAR(20) NOT NULL,
+    idEmpleado INT NOT NULL,
     createdAt DATE DEFAULT GETDATE()
 	, updatedAt DATE DEFAULT GETDATE()
 	, deletedAt DATE NULL
     
-    CONSTRAINT PK_Asistencia PRIMARY KEY (CodigoAsistencia),
-    CONSTRAINT CHK_Asistencia_Estado CHECK (EstadoAsistencia IN ('Presente', 'Ausente', 'Permiso', 'Incapacidad')),
-    CONSTRAINT FK_Asistencia_Empleados FOREIGN KEY (CodigoEmpleado) 
-        REFERENCES GestionEmpleados.Empleados(CodigoEmpleado)
-);
+    CONSTRAINT PK_Asistencia PRIMARY KEY (idAsistencia),
+    CONSTRAINT CHK_Asistencia_Estado CHECK (estado IN ('Presente', 'Ausente', 'Permiso', 'Incapacidad')),
+    CONSTRAINT FK_Asistencia_Empleados FOREIGN KEY (idEmpleado) 
+        REFERENCES GestionEmpleados.Empleados(idEmpleado)
+)
 GO
 
--- =========================================================================
--- 6. GESTIÓN DE EVALUACIONES DE DESEMPEÑO
--- =========================================================================
 CREATE TABLE GestionEmpresa.EvaluacionesDesempenio (
-    CodigoEvaluacion INT IDENTITY(1,1),
-    FechaEvaluacion DATE NOT NULL,
-    PeriodoEvaluado VARCHAR(50) NOT NULL,
-    CalificacionObtenida INT NOT NULL,
-    Comentarios VARCHAR(500) NULL,
-    CodigoEmpleado INT NOT NULL,
+    idEvaluacion INT IDENTITY(1,1),
+    fechaEvaluacion DATE NOT NULL,
+    periodoEvaluado VARCHAR(50) NOT NULL,
+    calificacionObtenida INT NOT NULL,
+    comentarios VARCHAR(500) NULL,
+    idEmpleado INT NOT NULL,
     createdAt DATE DEFAULT GETDATE()
 	, updatedAt DATE DEFAULT GETDATE()
 	, deletedAt DATE NULL
-    
-    CONSTRAINT PK_Evaluaciones PRIMARY KEY (CodigoEvaluacion),
-    CONSTRAINT CHK_Evaluacion_Calificacion CHECK (CalificacionObtenida BETWEEN 1 AND 100),
-    CONSTRAINT FK_Evaluaciones_Empleados FOREIGN KEY (CodigoEmpleado) 
-        REFERENCES GestionEmpleados.Empleados(CodigoEmpleado)
-);
+    CONSTRAINT PK_Evaluaciones PRIMARY KEY (idEvaluacion),
+    CONSTRAINT CHK_Evaluacion_Calificacion CHECK (calificacionObtenida BETWEEN 1 AND 100),
+    CONSTRAINT FK_Evaluaciones_Empleados FOREIGN KEY (idEmpleado) 
+        REFERENCES GestionEmpleados.Empleados(idEmpleado)
+)
 GO
 
 CREATE TABLE GestionCapacitaciones.Capacitaciones (
-    CodigoCapacitacion INT IDENTITY(1,1),
-    NombreCapacitacion VARCHAR(150) NOT NULL,
-    InstitucionResponsable VARCHAR(150) NOT NULL,
-    DuracionHoras INT NOT NULL,
-    FechaRealizacion DATE NOT NULL,
+    idCapacitacion INT IDENTITY(1,1),
+    nombre VARCHAR(150) NOT NULL,
+    institucionResponsable VARCHAR(150) NOT NULL,
+    duracionHoras INT NOT NULL,
+    fechaRealizacion DATE NOT NULL,
     createdAt DATE DEFAULT GETDATE()
 	, updatedAt DATE DEFAULT GETDATE()
 	, deletedAt DATE NULL
-    
-    CONSTRAINT PK_Capacitaciones PRIMARY KEY (CodigoCapacitacion),
-    CONSTRAINT CHK_Capacitacion_Duracion CHECK (DuracionHoras > 0)
-);
+    CONSTRAINT PK_Capacitaciones PRIMARY KEY (idCapacitacion),
+    CONSTRAINT CHK_Capacitacion_Duracion CHECK (duracionHoras > 0)
+)
 GO
 
 -- Tabla Intermedia: Empleado_Capacitación
 CREATE TABLE GestionCapacitaciones.Empleado_Capacitacion (
-    CodigoEmpleado INT NOT NULL,
-    CodigoCapacitacion INT NOT NULL,
-    FechaParticipacion DATE NOT NULL,
-    ResultadoObtenido VARCHAR(100) NULL,
+    idEmpleado INT NOT NULL,
+    idCapacitacion INT NOT NULL,
+    fechaParticipacion DATE NOT NULL,
+    resultadoObtenido VARCHAR(100) NULL,
     createdAt DATE DEFAULT GETDATE()
 	, updatedAt DATE DEFAULT GETDATE()
 	, deletedAt DATE NULL
     
-    CONSTRAINT PK_Empleado_Capacitacion PRIMARY KEY (CodigoEmpleado, CodigoCapacitacion),
-    CONSTRAINT FK_EmpCap_Empleados FOREIGN KEY (CodigoEmpleado) 
-        REFERENCES GestionEmpleados.Empleados(CodigoEmpleado),
-    CONSTRAINT FK_EmpCap_Capacitaciones FOREIGN KEY (CodigoCapacitacion) 
-        REFERENCES GestionCapacitaciones.Capacitaciones(CodigoCapacitacion)
-);
-GO
-
--- =========================================================================
--- 7. GESTIÓN DE CAPACITACIONES (Bajo esquema GestionCapacitaciones)
--- =========================================================================
-CREATE TABLE GestionCapacitaciones.Capacitaciones (
-    CodigoCapacitacion INT IDENTITY(1,1),
-    NombreCapacitacion VARCHAR(150) NOT NULL,
-    InstitucionResponsable VARCHAR(150) NOT NULL,
-    DuracionHoras INT NOT NULL,
-    FechaRealizacion DATE NOT NULL,
-    createdAt DATE DEFAULT GETDATE()
-	, updatedAt DATE DEFAULT GETDATE()
-	, deletedAt DATE NULL
-    
-    CONSTRAINT PK_Capacitaciones PRIMARY KEY (CodigoCapacitacion),
-    CONSTRAINT CHK_Capacitacion_Duracion CHECK (DuracionHoras > 0)
-);
-GO
-
--- Tabla Intermedia: Empleado_Capacitación
-CREATE TABLE GestionCapacitaciones.Empleado_Capacitacion (
-    CodigoEmpleado INT NOT NULL,
-    CodigoCapacitacion INT NOT NULL,
-    FechaParticipacion DATE NOT NULL,
-    ResultadoObtenido VARCHAR(100) NULL,
-    createdAt DATE DEFAULT GETDATE()
-	, updatedAt DATE DEFAULT GETDATE()
-	, deletedAt DATE NULL
-    
-    CONSTRAINT PK_Empleado_Capacitacion PRIMARY KEY (CodigoEmpleado, CodigoCapacitacion),
-    CONSTRAINT FK_EmpCap_Empleados FOREIGN KEY (CodigoEmpleado) 
-        REFERENCES GestionEmpleados.Empleados(CodigoEmpleado),
-    CONSTRAINT FK_EmpCap_Capacitaciones FOREIGN KEY (CodigoCapacitacion) 
-        REFERENCES GestionCapacitaciones.Capacitaciones(CodigoCapacitacion)
-);
+    CONSTRAINT PK_Empleado_Capacitacion PRIMARY KEY (idEmpleado, idCapacitacion),
+    CONSTRAINT FK_EmpCap_Empleados FOREIGN KEY (idEmpleado) 
+        REFERENCES GestionEmpleados.Empleados(idEmpleado),
+    CONSTRAINT FK_EmpCap_Capacitaciones FOREIGN KEY (idCapacitacion) 
+        REFERENCES GestionCapacitaciones.Capacitaciones(idCapacitacion)
+)
 GO
